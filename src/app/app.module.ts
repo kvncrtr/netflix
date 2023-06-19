07a126728f24f74709dff941b1169d1c12d71e68
Kevin Carter <console.logkevin@gmail.com>
2023-06-19 17:15:24 -0400

    add content and component for tv series page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { PreviewCardComponent } from './components/preview-card/preview-card.component';
+import { TvSeriesComponent } from './components/tv-series/tv-series.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     SearchBarComponent,
-    PreviewCardComponent
+    PreviewCardComponent,
+    TvSeriesComponent
   ],
   imports: [
     BrowserModule,
