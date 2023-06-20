77dd66a97eae348d1541cd5b7ff7d11698e1d28f
Shiko2875 <ssp2875@gmail.com>
2023-06-19 20:30:40 -0400

    Finished Styling for Mobile and Desktop View

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
