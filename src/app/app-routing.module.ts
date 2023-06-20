7a6cf5c6f4b589799316ca8883f99db0b3f2e5ad
Kevin Carter <console.logkevin@gmail.com>
2023-06-20 07:11:36 -0400

    add app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
+import { HomeComponent } from './components/home/home.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { TvSeriesComponent } from './components/tv-series/tv-series.component';
+import { BookmarkComponent } from './components/bookmark/bookmark.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'tv-series', component: TvSeriesComponent },
+  { path: 'bookmark', component: BookmarkComponent },
+  { path: '**', component: HomeComponent }
+
+];
+
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
