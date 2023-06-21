0544081ba31f927a96700d2d58540c92816ffe62
Kevin Carter <console.logkevin@gmail.com>
2023-06-21 17:12:25 -0400

    basic data pulled into the home page

--- src/app/components/preview-card/preview-card.component.ts
+++ src/app/components/preview-card/preview-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-preview-card',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./preview-card.component.css']
 })
 export class PreviewCardComponent {
+  @Input('cardData') cardData: any;
 
+  ngOnInit() {
+    console.log(this.cardData)
+  }
 }
