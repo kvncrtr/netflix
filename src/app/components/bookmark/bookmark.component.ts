a569e2508fdd84c24ba1a6fe3729a200486cd166
Shiko2875 <ssp2875@gmail.com>
2023-06-22 12:22:38 -0400

    Implemented Search Feature on Bookmarked Page

--- src/app/components/bookmark/bookmark.component.ts
+++ src/app/components/bookmark/bookmark.component.ts
@@ -3,16 +3,28 @@ import { Component } from '@angular/core';
 import { Media } from 'src/app/interfaces/media.interface';
 import { MediaService } from 'src/app/services/media.service';
 
+import { SearchService } from 'src/app/services/search.service';
+
 @Component({
   selector: 'app-bookmark',
   templateUrl: './bookmark.component.html',
   styleUrls: ['./bookmark.component.css']
 })
 export class BookmarkComponent {
+  searchTerm: string;
+  defaultBookmarkedMovies: any = [];
+  defaultBookmarkedSeries: any = [];
   bookmarkedMovies: any = []
   bookmarkedSeries: any = []
 
-  constructor(public mediaService: MediaService) {}
+  constructor(public mediaService: MediaService, private searchService: SearchService) {
+    this.searchTerm = this.searchService.getSearchTerm();
+
+    this.searchService.searchTerm$.subscribe(term => {
+      this.searchTerm = term;
+      this.filterData();
+    })
+  }
   
   ngOnInit(): void {
     this.getMedia()
@@ -20,15 +32,29 @@ export class BookmarkComponent {
   
   getMedia() : void {
     const subject = this.mediaService.fetchData();
-    
-    subject.subscribe((data: any) => {
-      data.reduce((prev: any, current: Media) => {
-        if(current.category === "Movie" && current.isBookmarked) {
-          this.bookmarkedMovies.push(current)
-        } else if(current.category === "TV Series" && current.isBookmarked) {
-          this.bookmarkedSeries.push(current)
-        }
-      }, [])
+
+    subject.subscribe(data => {
+      this.defaultBookmarkedMovies = data.filter(item => item.category === "Movie" && item.isBookmarked);
+      this.defaultBookmarkedSeries = data.filter(item => item.category === "TV Series" && item.isBookmarked);
+      this.filterData();
     })
+  }
+
+  filterData() {
+    if (this.searchTerm) {
+      this.bookmarkedMovies = this.defaultBookmarkedMovies.filter(item =>
+        item.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    } else {
+      this.bookmarkedMovies = this.defaultBookmarkedMovies;
+    }
+
+    if (this.searchTerm) {
+      this.bookmarkedSeries = this.defaultBookmarkedSeries.filter(item =>
+        item.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    } else {
+      this.bookmarkedSeries = this.defaultBookmarkedSeries;
+    }
   }
 }
