329d28c82b76bb4bf070ccf49e780d6dd5f10ac6
Shiko2875 <ssp2875@gmail.com>
2023-06-23 10:44:39 -0400

    Made it so trendingCard updatesOnView when clicking bookmark

--- src/app/components/trending-card/trending-card.component.ts
+++ src/app/components/trending-card/trending-card.component.ts
@@ -1,11 +1,58 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+import { Media } from 'src/app/interfaces/media.interface';
+import { MediaService } from 'src/app/services/media.service';
 
 @Component({
   selector: 'app-trending-card',
   templateUrl: './trending-card.component.html',
   styleUrls: ['./trending-card.component.css']
 })
-export class TrendingCardComponent {
+export class TrendingCardComponent implements OnInit {
   @Input('cardData') cardData: any;
-  trend: boolean = false;
+
+  bookmark: boolean;
+  bookmarkData: Media[] = []
+  nameClicked: string = '';
+  objectRelation: Media;
+  
+
+  constructor(
+    private mediaService: MediaService,
+    private router: Router) {}
+
+  ngOnInit() {
+    const subject = this.mediaService.fetchData()
+    this.bookmark = this.cardData.isBookmarked;
+    
+    subject.subscribe(data => {
+      this.bookmarkData = data
+    })
+  }  
+
+  handleSubscription(id: any, body: object) {
+    this.mediaService.patchNewBookmarkValue(id, body)
+      .subscribe(data => {
+        this.mediaService.switchMemory(data);
+        console.log("successfully handled server change!");
+      })
+  }
+
+  associateElementsObject():void {
+    const associatedObject = this.bookmarkData.find(media => media.title === this.nameClicked)
+    const id = associatedObject.id;
+    const body = {
+      ...associatedObject,
+      isBookmarked: !associatedObject.isBookmarked
+    }
+    this.objectRelation = associatedObject;
+    this.handleSubscription(id, body)
+  }
+
+  getElementName(event: any) {
+    this.nameClicked = event.target.attributes.name.value
+    this.associateElementsObject()
+  }
 }
