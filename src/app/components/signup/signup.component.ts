5c76d20cb818243751e78b90f03f47e8388f2d9c
Shiko2875 <ssp2875@gmail.com>
2023-06-24 08:08:17 -0400

    Finsihed SignUp Component functionality works. Gets users from Firebase and Posts if user's email is a new email.

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  userUrl: string =  "https://netflix-clone-fire-8079b-default-rtdb.firebaseio.com/Users";
+  jsonExt: string = '.json';
 
+  user: any = {
+    email: '',
+    password: '',
+    confirmPassword: ''
+  }
+  errorMessage: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  signup() {
+    if (this.user.password !== this.user.confirmPassword) {
+      alert("Please make sure your password matches");
+      return;
+    }
+
+    const userToSave = {
+      email: this.user.email,
+      password: this.user.password
+    };
+
+    this.http.get(`${this.userUrl}${this.jsonExt}`).subscribe(
+      (response: any) => {
+        const users = Object.values(response);
+        console.log(users);
+        const emailExists = users.some((user: any) => user.email === this.user.email);
+
+
+        if(emailExists) {
+          alert("Email already exists please use a different email address");
+        } else {
+          console.log("Email doesn't exist continue with post request");
+
+          this.http.post(`${this.userUrl}${this.jsonExt}`, userToSave).subscribe(
+            (postResponse: any) => {
+              console.log("User data have been saved sucessfully on Firebase:", postResponse);
+              alert("You have successfully created an account please go to login screen to login");
+              this.user.email = "";
+              this.user.password = "";
+              this.user.confirmPassword = "";
+            }, (postError) => {
+              console.error("Error saving user data:", postError);
+            }
+          )
+        } 
+      } , (error) => {
+        console.error("Error checking email existence:", error);
+      }
+    )
+  }
+  
 }
